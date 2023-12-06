1417ea2f7d88a61b6e951228b8c0313e0a45b099
RamyaMoraboina <ramya89780@gmail.com>
2023-12-06 16:56:08 +0530

    arrays

---
+++ arrays/practice.js
@@ -0,0 +1,21 @@
+//ARRAY OF OBJECTS 
+
+// const arr=[{firstname : "ramya",age:21,sex:"female"},  //here total content within curly braces is treated as single element
+// {firstname : "reema",age:18,sex:"female"},
+// {firstname : "vaibhav",age:15,sex:"male"}]; 
+
+
+
+// console.log(arr)
+//  console.log(arr[0])    //or arr['0']     //printing 0 index element
+//  console.log(arr['01']===arr['1'])  // false
+//  console.log(arr[0].firstname)  //printing firstname of 0 index element
+
+
+
+// length method
+const arr=[1,2,3]
+arr[6]="ramya"
+console.log(arr)
+console.log(arr.length)
+console.log(arr[5])
